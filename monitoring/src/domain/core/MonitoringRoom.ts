88cca75a2111f665a7464f57ccb34d1a224aa2a8
Mattia Matteini <mattia.matteini@studio.unibo.it>
2023-11-16 17:22:11 +0100

    setup import modules

--- monitoring/src/domain/core/MonitoringRoom.ts
+++ monitoring/src/domain/core/MonitoringRoom.ts
@@ -1,17 +1,15 @@
-import {Anomaly} from "domain";
+import {Anomaly} from "domain/core/Anomaly";
 
 interface MonitoringRoom {
     registerDevice(device: string): void;
+
     enableDevice(deviceId: number): void;
+
     disableDevice(deviceId: number): void;
+
     createSecurityRule(): void;
+
     deleteSecurityRule(): void;
+
     getHistory(deviceId: number): Anomaly[];
 }
-class Ciao {
-    constructor() {
-        var a: Anomaly = new Anomaly();
-    }
-}
-
-const test = new MonitoringRoom();

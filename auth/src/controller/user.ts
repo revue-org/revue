1d951ab46b70b11aacde25710c8a0de0bb3d6bd2
Alberto Paganelli <alberto.paganelli3@studio.unibo.it>
2023-11-29 10:01:47 +0100

    init authorization server

---
+++ auth/src/controller/user.ts
@@ -0,0 +1,11 @@
+import type { Request, Response } from 'express'
+
+export const userController = {
+  getUser: async (req: Request, res: Response) => {
+    /*try {
+      res.json(await deviceModel.findById(req.params.id))
+    } catch (err) {
+      console.log(err)
+    }*/
+  }
+}
---
+++ auth/src/index.ts
@@ -0,0 +1,37 @@
+import type { Express, Request, Response } from 'express'
+import express from 'express'
+
+import { config } from 'dotenv'
+import path, { dirname } from 'path'
+import { fileURLToPath } from 'url'
+import mongoose from 'mongoose'
+import { userRouter } from './routes/user.js'
+
+config()
+
+//export const __dirname: string = dirname(fileURLToPath(import.meta.url)) + '/../../'
+const app: Express = express()
+
+app.use(express.json())
+//app.use(express.static(path.join(__dirname, 'client')))
+
+const PORT: number = Number(process.env.PORT) || 4000
+
+app.use('/', userRouter)
+
+app.use((_: Request, res: Response) => {
+  res.status(404).send('<h1>404 Page Not Found!</h1>')
+})
+
+const mongoConnect = async () => {
+  try {
+    await mongoose.connect('mongodb://root:example@localhost:27017/auth?authSource=admin')
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+app.listen(PORT, () => {
+  console.log(`Authentication server listening on http://localhost:${PORT}`)
+  mongoConnect().then(r => console.log("connected to monitoring database"))
+})

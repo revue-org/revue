d1abdfe4ee87a55847e3f3d6a551dbaf9f4ca03f
Mattia Matteini <matteinimattia@gmail.com>
2023-12-14 16:48:21 +0100

    wip: add .env variables in build.gradle, add node-gradle plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
-//import com.github.gradle.node.task.NodeTask
+import com.github.gradle.node.npm.task.NpmTask
 
-group = "it.ldt" // Replace with your project's group
-version = "1.0"       // Replace with your project's version
+group = "it.ldt"
+version = "1.0"
 
 repositories {
     mavenCentral()
@@ -9,19 +9,36 @@ repositories {
 
 plugins {
     id("com.github.node-gradle.node") version "7.0.1"
+    id("co.uzzu.dotenv.gradle") version "3.0.0"
 }
+
 
 subprojects {
     apply(plugin = "com.github.node-gradle.node")
 
     node {
-        version = "20.10.0"
+        // versions useful only if download = true
+        download = true
+        version = env.NODE_VERSION.value
+        npmVersion = env.NPM_VERSION.value
+    }
+
+    tasks.register("install") {
+        dependsOn("npmInstall")
+    }
+    tasks.register("build") {
+        dependsOn(":${project.name}:npm_run_build")
     }
 
 }
 
 
-fun Project.registerNpmTask(taskName: String, npmCommand: String, dependencies: List<String> = emptyList(), isScript: Boolean = true) {
+fun Project.registerNpmTask(
+    taskName: String,
+    npmCommand: String,
+    dependencies: List<String> = emptyList(),
+    isScript: Boolean = true
+) {
     tasks.register(taskName, Exec::class) {
         val deps = if (npmCommand != "install") dependencies + ":${project.name}:npmInstall" else dependencies
         dependsOn(deps)
@@ -33,46 +50,46 @@ fun Project.registerNpmTask(taskName: String, npmCommand: String, dependencies: 
 // Domain module
 project(":domain") {
 //    registerNpmTask("install", "install", emptyList(), false)
-    registerNpmTask("build", "build")
-    registerNpmTask("test", "test")
-    registerNpmTask("format", "format")
-    registerNpmTask("format-fix", "format:fix")
-    registerNpmTask("lint", "lint")
+//    registerNpmTask("build", "build")
+//    registerNpmTask("test", "test")
+//    registerNpmTask("format", "format")
+//    registerNpmTask("format-fix", "format:fix")
+//    registerNpmTask("lint", "lint")
 }
 
 // Monitoring module
 project(":monitoring") {
 //    registerNpmTask("install", "install", emptyList(), false)
-    registerNpmTask("build", "build", listOf(":domain:build"))
-    registerNpmTask("start", "start")
-//    registerNpmTask("test", "test", listOf(":domain:build"))
-    registerNpmTask("format", "format")
-    registerNpmTask("format-fix", "format:fix")
-    registerNpmTask("lint", "lint")
-    tasks.register("deploy", Exec::class) {
-        commandLine("docker", "compose", "up", "-d", "--build", "--force-recreate")
-    }
+//    registerNpmTask("build", "build", listOf(":domain:build"))
+//    registerNpmTask("start", "start")
+////    registerNpmTask("test", "test", listOf(":domain:build"))
+//    registerNpmTask("format", "format")
+//    registerNpmTask("format-fix", "format:fix")
+//    registerNpmTask("lint", "lint")
+//    tasks.register("deploy", Exec::class) {
+//        commandLine("docker", "compose", "up", "-d", "--build", "--force-recreate")
+//    }
 }
 
 // Auth module
 project(":auth") {
 //    registerNpmTask("install", "install", emptyList(), false)
-    registerNpmTask("build", "build", listOf(":domain:build"))
-//    registerNpmTask("test", "test", listOf(":domain:build"))
-    registerNpmTask("format", "format")
-    registerNpmTask("format-fix", "format:fix")
-    registerNpmTask("lint", "lint")
-    tasks.register("deploy", Exec::class) {
-        commandLine("docker", "compose", "up", "-d", "--build", "--force-recreate")
-    }
+//    registerNpmTask("build", "build", listOf(":domain:build"))
+////    registerNpmTask("test", "test", listOf(":domain:build"))
+//    registerNpmTask("format", "format")
+//    registerNpmTask("format-fix", "format:fix")
+//    registerNpmTask("lint", "lint")
+//    tasks.register("deploy", Exec::class) {
+//        commandLine("docker", "compose", "up", "-d", "--build", "--force-recreate")
+//    }
 }
 
 
 // Client module
 project(":client") {
 //    registerNpmTask("install", "install", emptyList(), false)
-    registerNpmTask("build", "build", listOf(":domain:build"))
-    registerNpmTask("format-fix", "format:fix")
-    registerNpmTask("start", "start")
+//    registerNpmTask("build", "build", listOf(":domain:build"))
+//    registerNpmTask("format-fix", "format:fix")
+//    registerNpmTask("start", "start")
 //    registerNpmTask("test", "test", listOf(":domain:build"))
 }

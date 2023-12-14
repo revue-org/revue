f19ddc49934fe2b7747955e1e912faf8a5687329
Mattia Matteini <matteinimattia@gmail.com>
2023-12-14 15:21:07 +0100

    ci: reintroduce info on build

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,9 +7,23 @@ repositories {
     mavenCentral()
 }
 
+plugins {
+    id("com.github.node-gradle.node") version "7.0.1"
+}
+
+subprojects {
+    apply(plugin = "com.github.node-gradle.node")
+
+    node {
+        version = "20.10.0"
+    }
+
+}
+
+
 fun Project.registerNpmTask(taskName: String, npmCommand: String, dependencies: List<String> = emptyList(), isScript: Boolean = true) {
     tasks.register(taskName, Exec::class) {
-        val deps = if (npmCommand != "install") dependencies + ":${project.name}:install" else dependencies
+        val deps = if (npmCommand != "install") dependencies + ":${project.name}:npmInstall" else dependencies
         dependsOn(deps)
         if (isScript) commandLine("npm", "run", npmCommand)
         else commandLine("npm", npmCommand)
@@ -18,7 +32,7 @@ fun Project.registerNpmTask(taskName: String, npmCommand: String, dependencies: 
 
 // Domain module
 project(":domain") {
-    registerNpmTask("install", "install", emptyList(), false)
+//    registerNpmTask("install", "install", emptyList(), false)
     registerNpmTask("build", "build")
     registerNpmTask("test", "test")
     registerNpmTask("format", "format")
@@ -28,7 +42,7 @@ project(":domain") {
 
 // Monitoring module
 project(":monitoring") {
-    registerNpmTask("install", "install", emptyList(), false)
+//    registerNpmTask("install", "install", emptyList(), false)
     registerNpmTask("build", "build", listOf(":domain:build"))
     registerNpmTask("start", "start")
 //    registerNpmTask("test", "test", listOf(":domain:build"))
@@ -42,7 +56,7 @@ project(":monitoring") {
 
 // Auth module
 project(":auth") {
-    registerNpmTask("install", "install", emptyList(), false)
+//    registerNpmTask("install", "install", emptyList(), false)
     registerNpmTask("build", "build", listOf(":domain:build"))
 //    registerNpmTask("test", "test", listOf(":domain:build"))
     registerNpmTask("format", "format")
@@ -56,7 +70,7 @@ project(":auth") {
 
 // Client module
 project(":client") {
-    registerNpmTask("install", "install", emptyList(), false)
+//    registerNpmTask("install", "install", emptyList(), false)
     registerNpmTask("build", "build", listOf(":domain:build"))
     registerNpmTask("format-fix", "format:fix")
     registerNpmTask("start", "start")

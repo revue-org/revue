a798da85fda757b9cbaec4bf5dabfacf67fd37e1
Mattia Matteini <matteinimattia@gmail.com>
2023-12-15 15:49:32 +0100

    other: add tasks with gradle node plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,3 @@
-import com.github.gradle.node.npm.task.NpmTask
-
 group = "it.ldt"
 version = "1.0"
 
@@ -9,87 +7,44 @@ repositories {
 
 plugins {
     id("com.github.node-gradle.node") version "7.0.1"
-    id("co.uzzu.dotenv.gradle") version "3.0.0"
+//    id("co.uzzu.dotenv.gradle") version "3.0.0"
 }
 
-
 subprojects {
+
+
     apply(plugin = "com.github.node-gradle.node")
 
     node {
         // versions useful only if download = true
         download = true
-        version = env.NODE_VERSION.value
-        npmVersion = env.NPM_VERSION.value
+        version = "20.10.0"   // env.NODE_VERSION.value
+        npmVersion = "10.2.3" // env.NPM_VERSION.value
     }
 
     tasks.register("install") {
-        dependsOn("npmInstall")
+        dependsOn(":npmInstall")
     }
     tasks.register("build") {
         dependsOn(":${project.name}:npm_run_build")
+        dependsOn(":${project.name}:npmInstall")
+        dependsOn(":domain:npm_run_build")
+    }
+    tasks.register("test") {
+        dependsOn(":${project.name}:npm_run_build")
+        dependsOn(":${project.name}:npm_run_test")
+    }
+    tasks.register("format") {
+        dependsOn(":${project.name}:npm_run_format")
+    }
+    tasks.register("lint") {
+        dependsOn(":${project.name}:npm_run_lint")
     }
 
-}
-
-
-fun Project.registerNpmTask(
-    taskName: String,
-    npmCommand: String,
-    dependencies: List<String> = emptyList(),
-    isScript: Boolean = true
-) {
-    tasks.register(taskName, Exec::class) {
-        val deps = if (npmCommand != "install") dependencies + ":${project.name}:npmInstall" else dependencies
-        dependsOn(deps)
-        if (isScript) commandLine("npm", "run", npmCommand)
-        else commandLine("npm", npmCommand)
+    // ordering task execution
+    if (project.name != "domain") {
+        tasks.findByPath(":${project.name}:npmInstall")?.mustRunAfter(":domain:npm_run_build")
     }
-}
-
-// Domain module
-project(":domain") {
-//    registerNpmTask("install", "install", emptyList(), false)
-//    registerNpmTask("build", "build")
-//    registerNpmTask("test", "test")
-//    registerNpmTask("format", "format")
-//    registerNpmTask("format-fix", "format:fix")
-//    registerNpmTask("lint", "lint")
-}
-
-// Monitoring module
-project(":monitoring") {
-//    registerNpmTask("install", "install", emptyList(), false)
-//    registerNpmTask("build", "build", listOf(":domain:build"))
-//    registerNpmTask("start", "start")
-////    registerNpmTask("test", "test", listOf(":domain:build"))
-//    registerNpmTask("format", "format")
-//    registerNpmTask("format-fix", "format:fix")
-//    registerNpmTask("lint", "lint")
-//    tasks.register("deploy", Exec::class) {
-//        commandLine("docker", "compose", "up", "-d", "--build", "--force-recreate")
-//    }
-}
-
-// Auth module
-project(":auth") {
-//    registerNpmTask("install", "install", emptyList(), false)
-//    registerNpmTask("build", "build", listOf(":domain:build"))
-////    registerNpmTask("test", "test", listOf(":domain:build"))
-//    registerNpmTask("format", "format")
-//    registerNpmTask("format-fix", "format:fix")
-//    registerNpmTask("lint", "lint")
-//    tasks.register("deploy", Exec::class) {
-//        commandLine("docker", "compose", "up", "-d", "--build", "--force-recreate")
-//    }
-}
-
-
-// Client module
-project(":client") {
-//    registerNpmTask("install", "install", emptyList(), false)
-//    registerNpmTask("build", "build", listOf(":domain:build"))
-//    registerNpmTask("format-fix", "format:fix")
-//    registerNpmTask("start", "start")
-//    registerNpmTask("test", "test", listOf(":domain:build"))
+    tasks.findByPath(":${project.name}:npm_run_build")?.mustRunAfter(":${project.name}:npmInstall")
+    tasks.findByPath(":${project.name}:npmInstall")?.doNotTrackState("error in ci")
 }

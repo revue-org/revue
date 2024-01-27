31ce442c57248d5aeb72f6fc80a748a62a616aa4
Alberto Paganelli <alberto.paganelli3@studio.unibo.it>
2024-01-27 17:55:14 +0100

    chore: add security rule data in setup script of alarm database

--- alarm/db/alarm-init.js
+++ alarm/db/alarm-init.js
@@ -64,3 +64,83 @@ db.notification.insert([
     __v: 0
   }
 ])
+
+db.createCollection('securityRule')
+db.securityRule.insert([
+  {
+    _id: {
+      $oid: '65b527590fa38e9a5422537c'
+    },
+    deviceId: {
+      type: 'SENSOR',
+      code: 'sen-01'
+    },
+    creatorId: {
+      $oid: '6582b78ee645d6402a3be6e2' // qui andra messo l'id dell'utente inserito nello script di auth db
+    },
+    description: 'Exceeding rule description',
+    minValue: 0,
+    maxValue: 25,
+    measure: 'TEMPERATURE',
+    contacts: [
+      {
+        type: 'SMS',
+        value: '3333333333',
+        _id: {
+          $oid: '65b527590fa38e9a5422537d'
+        }
+      },
+      {
+        type: 'EMAIL',
+        value: 'email@gmail.com',
+        _id: {
+          $oid: '65b527590fa38e9a5422537e'
+        }
+      }
+    ],
+    from: {
+      $date: '2018-01-01T01:00:00.000Z'
+    },
+    to: {
+      $date: '2020-01-01T01:00:00.000Z'
+    },
+    __v: 0
+  },
+  {
+    _id: {
+      $oid: '65b52e53cffd8e469604ef10'
+    },
+    deviceId: {
+      type: 'CAMERA',
+      code: 'cam-01'
+    },
+    creatorId: {
+      $oid: '6582b78ee645d6402a3be6e2'
+    },
+    description: 'Intrusion rule description',
+    objectClass: 'PERSON',
+    contacts: [
+      {
+        type: 'SMS',
+        value: '3333333333',
+        _id: {
+          $oid: '65b52e53cffd8e469604ef11'
+        }
+      },
+      {
+        type: 'EMAIL',
+        value: 'email@gmail.com',
+        _id: {
+          $oid: '65b52e53cffd8e469604ef12'
+        }
+      }
+    ],
+    from: {
+      $date: '2018-01-01T01:00:00.000Z'
+    },
+    to: {
+      $date: '2020-01-01T01:00:00.000Z'
+    },
+    __v: 0
+  }
+])

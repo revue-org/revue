41b613a7c63229759957a4bec2a3b4dffecd9fa4
Alberto Paganelli <alberto.paganelli3@studio.unibo.it>
2024-02-02 11:06:36 +0100

    ci: test bash

--- frontend/src/components/devices/SensorData.vue
+++ frontend/src/components/devices/SensorData.vue
@@ -68,12 +68,12 @@ watch(sensorData, newSensorData => {
         },
         {
           label: 'Humidity',
-          borderColor: 'orange',
+          borderColor: 'teal',
           data: toRaw(bufferStore.humidityBuffer) as never[]
         },
         {
           label: 'Pressure',
-          borderColor: 'teal',
+          borderColor: 'orange',
           data: toRaw(bufferStore.pressureBuffer) as never[]
         }
       ]
@@ -91,13 +91,13 @@ const chartData = ref({
     },
     {
       label: 'Humidity',
-      borderColor: 'orange',
+      borderColor: 'teal',
       data: [],
       hidden: true
     },
     {
       label: 'Pressure',
-      borderColor: 'teal',
+      borderColor: 'orange',
       data: [],
       hidden: true
     }

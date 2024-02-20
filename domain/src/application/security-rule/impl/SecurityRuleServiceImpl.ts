494011e7c34cdd8ca38f49cb625899d5fd5aeb23
Alberto Paganelli <alberto.paganelli3@studio.unibo.it>
2024-02-20 16:38:37 +0100

    wip: green test for exceeding rule active detection

--- domain/src/application/security-rule/impl/SecurityRuleServiceImpl.ts
+++ domain/src/application/security-rule/impl/SecurityRuleServiceImpl.ts
@@ -1,16 +1,18 @@
-import { SecurityRuleService } from "../SecurityRuleService";
-import { SecurityRule } from "../../../domain/security-rule/core";
-import { EnvironmentData } from "../../../domain/device/core";
-
+import { SecurityRuleService } from '../SecurityRuleService'
+import { ExceedingRule, IntrusionRule, SecurityRule } from '../../../domain/security-rule/core'
+import { DeviceType, EnvironmentData } from '../../../domain/device/core'
 
 export class SecurityRuleServiceImpl implements SecurityRuleService {
   private securityRules: SecurityRule[] = []
+
   addSecurityRule(securityRule: SecurityRule): void {
     this.securityRules.push(securityRule)
   }
 
   removeSecurityRule(securityRuleId: string): void {
-    this.securityRules = this.securityRules.filter((rule: SecurityRule) => rule.securityRuleId !== securityRuleId)
+    this.securityRules = this.securityRules.filter(
+      (rule: SecurityRule) => rule.securityRuleId !== securityRuleId
+    )
   }
 
   updateSecurityRule(securityRule: SecurityRule): void {
@@ -22,16 +24,37 @@ export class SecurityRuleServiceImpl implements SecurityRuleService {
     })
   }
 
+  getActiveRules(): SecurityRule[] {
+    return this.securityRules.filter((rule: SecurityRule) => rule.from < new Date() && rule.to > new Date())
+  }
+
+  getActiveExceedingRules(): ExceedingRule[] {
+    return this.securityRules.filter(
+      (rule: SecurityRule) =>
+        rule.from < new Date() && rule.to > new Date() && rule.deviceId.type === DeviceType.SENSOR
+    ) as ExceedingRule[]
+  }
+
+  getActiveIntrusionRules(): IntrusionRule[] {
+    return this.securityRules.filter(
+      (rule: SecurityRule) =>
+        rule.from < new Date() && rule.to > new Date() && rule.deviceId.type === DeviceType.CAMERA
+    ) as IntrusionRule[]
+  }
+
   checkExceedingDetection(environmentData: EnvironmentData): boolean {
-
-    return true
-
-
+    return (
+      this.getActiveExceedingRules().filter(
+        (rule: ExceedingRule) =>
+          rule.deviceId.code === environmentData.sourceDeviceId.code &&
+          rule.measure === environmentData.measure &&
+          (environmentData.value < rule.min || environmentData.value > rule.max)
+      ).length > 0
+    )
   }
 
   checkIntrusionDetection(detection: any): boolean {
     //TODO TO IMPLEMENT
-    return false;
+    return false
   }
-
 }

212c864c8f7cf4bcd351c7203f842ddf55b74601
Mattia Matteini <matteinimattia@gmail.com>
2024-03-01 11:22:50 +0100

    feat(recognition): add logger, request to alarm, env variables

--- recognition/app/__init__.py
+++ recognition/app/__init__.py
@@ -1,11 +1,23 @@
+import logging
+import os
 import threading
-import time
 from concurrent.futures import ThreadPoolExecutor
+from os.path import join, dirname
 
+import requests
+from dotenv import load_dotenv
 from flask import Flask
 
 from app.Recognizer import Recognizer
 
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger('recognition')
+
+dotenv_path = join(dirname(__file__), '../../.env')
+load_dotenv(dotenv_path)
+ALARM_PORT = os.environ.get("ALARM_PORT")
+ALARM_HOST = 'localhost'# os.environ.get("ALARM_HOST")
+DEV_API_KEY = os.environ.get("DEV_API_KEY")
 executor = ThreadPoolExecutor()
 
 
@@ -21,6 +33,13 @@ def set_interval(func, sec) -> threading.Timer:
 
 def create_app():
     app = Flask(__name__)
+
+    url = f'http://{ALARM_HOST}:{ALARM_PORT}/security-rules/intrusions'
+    headers = {'Authorization': f'Bearer {DEV_API_KEY}'}
+    logger.info(url)
+    logger.info(headers)
+    r = requests.get(url, headers=headers)
+    logger.info(r.json())
 
     @app.route("/")
     def hello_world():

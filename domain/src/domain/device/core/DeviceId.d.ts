4bb1ca2f8d0562ad5eb98ce9fd8bc2f4e5066f8b
Alberto Paganelli <alberto.paganelli3@studio.unibo.it>
2024-03-11 16:22:24 +0100

    other: remove typos

--- domain/src/domain/device/core/DeviceId.d.ts
+++
@@ -1,7 +0,0 @@
-import { DeviceType } from './impl/enum/DeviceType.js'
-export interface DeviceId {
-  get type(): DeviceType
-  set type(type: DeviceType)
-  get code(): string
-  set code(code: string)
-}

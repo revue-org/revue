dc6926c216258502cc4a90445b1585cef0f2013e
Mattia Matteini <matteinimattia@gmail.com>
2024-04-03 10:32:47 +0200

    refactor: update distribution test script

--- distribution-tests.sh
+++ distribution-tests.sh
@@ -1,17 +1,30 @@
+function tear_down() {
+    ./undeploy.sh
+    if [ "$1" -ne 0 ]; then
+        echo "Tests of $2 failed. Exiting."
+        exit "$1"
+    else
+        echo "Tests passed successfully."
+    fi
+}
+
+function execute_test() {
+    SERVICE=$1
+    ./scripts/compose.sh run --rm --name "$SERVICE"-test revue-"$SERVICE" npm test
+    EXIT_CODE=$?
+    if [ $EXIT_CODE -ne 0 ]; then
+        tear_down $EXIT_CODE "monitoring"
+    fi
+}
+
 ./deploy.sh
 sleep 5
 
-./scripts/compose.sh down recognition
+./scripts/compose.sh down revue-recognition
 
 sleep 5
 
-./scripts/compose.sh run --rm --name monitoring-test revue-monitoring npm test
-EXIT_CODE=$?
-if [ $EXIT_CODE -ne 0 ]; then
-    echo "Tests of monitoring failed. Exiting."
-    exit $EXIT_CODE
-fi
+execute_test "monitoring"
 
-./undeploy.sh
+tear_down 0
 
-echo "Tests passed successfully."

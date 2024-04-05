8f87ad4bb57924608ed5960e47b278c4314aefa5
Mattia Matteini <matteinimattia@gmail.com>
2024-04-05 16:40:04 +0200

    fix: frontend env variables and cors origin

--- notification/src/index.ts
+++ notification/src/index.ts
@@ -19,11 +19,9 @@ const PORT: number = Number(process.env.NOTIFICATION_PORT) || 4002
 
 const server: HttpServer = http.createServer(app)
 
-const frontendHost: string = process.env.FRONTEND_HOST || 'localhost'
-const frontendPort: string = process.env.FRONTEND_PORT || '8080'
 export const io: SocketIOServer = new SocketIOServer(server, {
   cors: {
-    origin: `http://${frontendHost}:${frontendPort}`
+    origin: '*'
   }
 })
 

3208af74b85e1a665650f3629d35d479d5eae616
Mattia Matteini <matteinimattia@gmail.com>
2024-04-16 15:56:06 +0200

    refactor(recognition): add dynamic change of env variables

--- recognition/app/utils/env.py
+++ recognition/app/utils/env.py
@@ -5,8 +5,8 @@ from dotenv import load_dotenv
 
 dotenv_path = join(dirname(__file__), "../../../.env")
 load_dotenv(dotenv_path)
-ALARM_HOST = "localhost"  # os.environ.get("ALARM_HOST")
+ALARM_HOST = "localhost" if os.environ.get("FLASK_ENV") == "develop" else os.environ.get("ALARM_HOST")
 ALARM_PORT = os.environ.get("ALARM_PORT")
-KAFKA_HOST = "localhost"  # os.environ.get("KAFKA_HOST")
-KAFKA_PORT = "9094"  # os.environ.get("KAFKA_PORT")
+KAFKA_HOST = "localhost" if os.environ.get("FLASK_ENV") == "develop" else os.environ.get("KAFKA_HOST")
+KAFKA_PORT = "9094" if os.environ.get("FLASK_ENV") == "develop" else os.environ.get("KAFKA_PORT")
 RECOGNITION_BEARER_TOKEN = os.environ.get("RECOGNITION_BEARER_TOKEN")

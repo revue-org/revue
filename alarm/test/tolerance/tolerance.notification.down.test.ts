f1c9df74105d260c0b592225fbc12613188253b7
Alberto Paganelli <alberto.paganelli3@studio.unibo.it>
2024-04-17 17:35:14 +0200

    other: change sec to ms to notification tolerance test

--- alarm/test/tolerance/tolerance.notification.down.test.ts
+++ alarm/test/tolerance/tolerance.notification.down.test.ts
@@ -45,7 +45,7 @@ describe(`When notification service is down`, (): void => {
     const actualAnomalies: number = (await anomalyService.getExceedings()).length
     await new Promise(
       (resolve): NodeJS.Timeout =>
-        setTimeout(resolve, intervals.reduce((acc: number, curr: number) => acc + curr, 0) + 2)
+        setTimeout(resolve, intervals.reduce((acc: number, curr: number) => acc + curr, 0) + 2000)
     )
 
     const newAnomalies: number = (await anomalyService.getExceedings()).length

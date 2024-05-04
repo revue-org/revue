e0cde29976fb9c721936c25c47fb6c6c391fbbbf
Mattia Matteini <matteinimattia@gmail.com>
2024-05-04 16:13:33 +0200

    build: add script to create k8s files

---
+++ scripts/create-k8s-config.sh
@@ -0,0 +1,21 @@
+docker compose \
+    --project-name revue \
+    --project-directory . \
+    -f auth/docker-compose.yml \
+    -f kafka/docker-compose.yml \
+    -f monitoring/docker-compose.yml \
+    -f alarm/docker-compose.yml \
+    -f media-server/docker-compose.yml \
+    -f frontend/docker-compose.yml \
+    -f log/docker-compose.yml \
+    -f notification/docker-compose.yml \
+    -f camera/docker-compose.yml \
+    -f sensor/docker-compose.yml \
+    -f recognition/docker-compose.yml \
+    config > all-docker-compose.yml
+
+kompose convert \
+  -f all-docker-compose.yml \
+  -o k8s
+
+rm all-docker-compose.yml

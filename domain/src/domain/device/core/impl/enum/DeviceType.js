14e3edb3b80cd1a91d704a06327ffa72f6b1cd4b
Alberto Paganelli <alberto.paganelli3@studio.unibo.it>
2024-06-07 12:06:14 +0200

    docs: add open api specifications for log microservice

--- domain/src/domain/device/core/impl/enum/DeviceType.js
+++
@@ -1,5 +0,0 @@
-export var DeviceType
-;(function (DeviceType) {
-  DeviceType[(DeviceType['CAMERA'] = 0)] = 'CAMERA'
-  DeviceType[(DeviceType['SENSOR'] = 1)] = 'SENSOR'
-})(DeviceType || (DeviceType = {}))

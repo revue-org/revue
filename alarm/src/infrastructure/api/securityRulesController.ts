9539fee41860a569bfe9ec83b4b9aa49feadcecf
Kelvin Olaiya <kelvinoluwada.olaiya@studio.unibo.it>
2024-06-08 22:03:43 +0200

    refactor(alarm): partially implement alarm rest api

---
+++ alarm/src/infrastructure/api/securityRulesController.ts
@@ -0,0 +1,115 @@
+import { Measure } from '@domain/device/core/impl/enum/Measure.js'
+import { DeviceId } from '@domain/device/core/DeviceId.js'
+import { Contact } from '@domain/monitoring/core/Contact.js'
+import { SecurityRule } from '@/domain/core/rules/SecurityRule'
+import { SecurityRuleService } from '@/application/services/SecurityRuleService'
+import { SecurityRuleServiceImpl } from '@/application/services/SecurityRuleServiceImpl'
+import { SecurityRulesFactory } from '@/domain/factories/SecurityRulesFactory'
+import { RangeRule } from '@/domain/core/rules/RangeRule'
+import { IntrusionRule } from '@/domain/core/rules/IntrusionRule'
+
+const service: SecurityRuleService = new SecurityRuleServiceImpl(new SecurityRuleRepositoryImpl())
+
+export const securityRuleController = {
+
+  getSecurityRuleById: async (id: string): Promise<SecurityRule> => {
+    return await service.getSecurityRuleById(id)
+  },
+
+  getRangeRules: async (): Promise<RangeRule[]> => {
+    return await service.getRangeRules()
+  },
+
+  getIntrusionRules: async (): Promise<IntrusionRule[]> => {
+    return await service.getIntrusionRules()
+  },
+
+  createRangeRule: async (
+    deviceId: DeviceId,
+    creatorId: string,
+    description: string,
+    measure: Measure,
+    minValue: number,
+    maxValue: number,
+    from: Date,
+    to: Date,
+    contacts: Contact[]
+  ): Promise<void> => {
+    service.createRangeRule(
+      SecurityRulesFactory.createRangeRule(
+        new SecurityRuleId(),
+        deviceId,
+        creatorId,
+        contacts,
+        description,
+        new TimeSlot(from, to),
+        minValue,
+        maxValue,
+        measure,
+        true
+      )
+    )
+  },
+
+  createIntrusionRule: async (
+    deviceId: DeviceId,
+    creatorId: string,
+    description: string,
+    objectClass: ObjectClass,
+    from: Date,
+    to: Date,
+    contacts: Contact[]
+  ): Promise<void> => {
+    service.createIntrusionRule(
+      SecurityRulesFactory.createIntrusionRule(
+        new SecurityRuleId(),
+        deviceId,
+        creatorId,
+        objectClass,
+        contacts,
+        description,
+        new TimeSlot(from, to),
+        true
+
+      )
+    )
+  },
+
+  updateRangeRule: async (
+    exceedingRuleId: string,
+    deviceId: DeviceId,
+    description: string,
+    measure: Measure,
+    minValue: number,
+    maxValue: number,
+    from: Date,
+    to: Date,
+    contacts: Contact[]
+  ): Promise<void> => {
+    service.updateRangeRule(
+      SecurityRulesFactory.createRangeRule(
+
+      )
+    )
+  },
+
+  updateIntrusionRule: async (
+    intrusionRuleId: string,
+    deviceId: DeviceId,
+    description: string,
+    objectClass: ObjectClass,
+    from: Date,
+    to: Date,
+    contacts: Contact[]
+  ): Promise<void> => {
+    service.updateIntrusionRule(
+      SecurityRulesFactory.createIntrusionRule(
+
+      )
+    )
+  },
+
+  deleteSecurityRule: async (id: string): Promise<void> => {
+    return service.deleteSecurityRule(id)
+  },
+}
---
+++ alarm/src/infrastructure/api/securityRulesRouter.ts
@@ -0,0 +1,137 @@
+import HttpStatusCode from "domain/dist/utils/HttpStatusCode"
+import express, { Router, Request, Response } from "express"
+import { securityRuleController as controller } from "./securityRulesController"
+import { SecurityRule } from "@/domain/core/rules/SecurityRule"
+import { RangeRule } from "@/domain/core/rules/RangeRule"
+
+export const router: Router = express.Router()
+const deviceIdFactory: DeviceIdFactory = new DeviceIdFactoryImpl()
+
+router.route('/:id')
+    .get((req: Request, res: Response): void => {
+        controller.getSecurityRuleById(req.params.id)
+            .then((securityRule: SecurityRule): void => {
+                res.status(HttpStatusCode.OK).send(securityRule)
+            })
+            .catch((): void => {
+                res.send({ error: 'No security rule found' })
+            })
+    })
+
+router.route('/exceedings')
+    .get((req: Request, res: Response): void => {
+        controller.getRangeRules()
+            .then((exceedingRules: RangeRule[]): void => {
+                res.status(HttpStatusCode.OK).send(exceedingRules)
+            })
+            .catch((): void => {
+                res.send({ error: 'No exceeding rules found' })
+            })
+    })
+    .post((req: Request, res: Response): void => {
+
+        controller.createRangeRule(
+            deviceIdFactory.createSensorId(req.body.deviceId.code),
+            req.body.creatorId,
+            req.body.description,
+            MeasureConverter.convertToMeasure(req.body.measure),
+            req.body.min,
+            req.body.max,
+            new Date(req.body.from),
+            new Date(req.body.to),
+            req.body.contacts
+        )
+            .then((): void => {
+                res.status(HttpStatusCode.CREATED).send({ success: 'Exceeding rule created' })
+            })
+            .catch((): void => {
+                res.send({ error: 'Exceeding rule not created' })
+            })
+    })
+    .put((req: Request, res: Response): void => {
+        controller.updateRangeRule(
+            req.body.id,
+            deviceIdFactory.createSensorId(req.body.deviceId.code),
+            req.body.description,
+            MeasureConverter.convertToMeasure(req.body.measure),
+            req.body.min,
+            req.body.max,
+            new Date(req.body.from),
+            new Date(req.body.to),
+            req.body.contacts
+        )
+            .then((): void => {
+                res.status(HttpStatusCode.OK).send({ success: 'Exceeding rule updated' })
+            })
+            .catch((): void => {
+                res.send({ error: 'Exceeding rule not updated' })
+            })
+    })
+
+router.route('/exceedings/:id').delete((req: Request, res: Response): void => {
+    controller.deleteSecurityRule(req.params.id)
+        .then((): void => {
+            res.status(HttpStatusCode.OK).send({ success: 'Exceeding rule correctly deleted' })
+        })
+        .catch((): void => {
+            res.send({ error: 'Exceeding rule not deleted' })
+        })
+})
+
+
+router.route('/intrusions')
+    .get((req: Request, res: Response): void => {
+        controller.getIntrusionRules()
+            .then((intrusionRules: IntrusionRule[]): void => {
+                res.status(HttpStatusCode.OK).send(intrusionRules)
+            })
+            .catch((): void => {
+                res.send({ error: 'No intrusion rules found' })
+            })
+    })
+    .post((req: Request, res: Response): void => {
+        controller.createIntrusionRule(
+            deviceIdFactory.createCameraId(req.body.deviceId.code),
+            req.body.creatorId,
+            req.body.description,
+            ObjectClassConverter.convertToObjectClass(req.body.objectClass),
+            new Date(req.body.from),
+            new Date(req.body.to),
+            req.body.contacts
+        )
+            .then((): void => {
+                res.status(HttpStatusCode.CREATED).send({ success: 'Intrusion rule created' })
+            })
+            .catch((err): void => {
+                console.log(err)
+                res.send({ error: 'Intrusion rule not created' })
+            })
+    })
+    .put((req: Request, res: Response): void => {
+        controller.updateIntrusionRule(
+            req.body.id,
+            deviceIdFactory.createCameraId(req.body.deviceId.code),
+            req.body.description,
+            ObjectClassConverter.convertToObjectClass(req.body.objectClass),
+            new Date(req.body.from),
+            new Date(req.body.to),
+            req.body.contacts
+        )
+            .then((): void => {
+                res.status(HttpStatusCode.OK).send({ success: 'Intrusion rule updated' })
+            })
+            .catch((): void => {
+                res.send({ error: 'Intrusion rule not updated' })
+            })
+    })
+
+router.route('/intrusions/:id')
+    .delete((req: Request, res: Response): void => {
+        controller.deleteSecurityRule(req.params.id)
+            .then((): void => {
+                res.status(HttpStatusCode.OK).send({ success: 'Intrusion rule correctly deleted' })
+            })
+            .catch((): void => {
+                res.send({ error: 'Intrusion rule not deleted' })
+            })
+    })

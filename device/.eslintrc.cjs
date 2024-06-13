c0cfb4686def5e1af23a1929db6703da19f820eb
Kelvin Olaiya <kelvinoluwada.olaiya@studio.unibo.it>
2024-06-13 10:53:39 +0200

    chore: add files for build configuration in location and device microservices

---
+++ device/.eslintrc.cjs
@@ -0,0 +1,24 @@
+require('@rushstack/eslint-patch/modern-module-resolution')
+
+module.exports = {
+  root: true,
+  env: {
+    'node': true
+  },
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'eslint:recommended'
+  ],
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'off',
+    'no-unused-vars': ['error', { 'argsIgnorePattern': '^_' }],
+    '@typescript-eslint/no-unused-vars': ['error', { 'argsIgnorePattern': '^_' }]
+  },
+  // exclude dist directory
+  ignorePatterns: ['dist/']
+}

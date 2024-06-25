604b4dbf0f8673a465a294fcce4d39782896efce
Mattia Matteini <matteinimattia@gmail.com>
2024-06-25 23:07:38 +0200

    build: add docker compose for location, add device and location in build scripts

--- scripts/compose.sh
+++ scripts/compose.sh
@@ -2,12 +2,15 @@ docker compose \
     --project-name revue \
     --project-directory . \
     -f auth/docker-compose.yml \
+    -f user/docker-compose.yml \
     -f kafka/docker-compose.yml \
     -f monitoring/docker-compose.yml \
+    -f device/docker-compose.yml \
     -f alarm/docker-compose.yml \
     -f media-server/docker-compose.yml \
     -f frontend/docker-compose.yml \
     -f log/docker-compose.yml \
+    -f location/docker-compose.yml \
     -f notification/docker-compose.yml \
     -f camera/docker-compose.yml \
     -f sensor/docker-compose.yml \

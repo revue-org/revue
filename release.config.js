016e4dc5301c5f214e760eaa182b5e6417ad5679
Kelvin Olaiya <kelvinoluwada.olaiya@studio.unibo.it>
2024-07-01 11:11:18 +0200

    ci(release): write release command in a separete variable

--- release.config.js
+++ release.config.js
@@ -1,9 +1,12 @@
 const config = require('semantic-release-preconfigured-conventional-commits')
+const publishCmd = `
+echo "not releasing yet";
+cat CHANGELOG.md'
+`
 config.plugins.push(
   [
     '@semantic-release/exec', {
-      publishCmd: 'echo "not releasing yet" /' +
-          'cat CHANGELOG.md /'
+      publishCmd: publishCmd
   }],
   '@semantic-release/github',
   '@semantic-release/git'

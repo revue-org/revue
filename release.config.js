c97386ea742d8d9b46988f16046fddcd9bec4e24
Kelvin Olaiya <kelvinoluwada.olaiya@studio.unibo.it>
2024-07-01 10:59:24 +0200

    ci(release): remove useless lines

--- release.config.js
+++ release.config.js
@@ -1,7 +1,4 @@
 const config = require('semantic-release-preconfigured-conventional-commits')
-// this already includes:
-// - semantic-release/commit-analyzer
-// - semantic-release/release-notes-generator
 config.plugins.push(
   // [
   //   '@semantic-release/exec',

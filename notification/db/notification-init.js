ebc13de705671ba21e20479b7052f06bbb156fc4
Alberto Paganelli <alberto.paganelli3@studio.unibo.it>
2024-07-01 10:12:40 +0200

    chore: add notification sample in init db

--- notification/db/notification-init.js
+++ notification/db/notification-init.js
@@ -1,3 +1,16 @@
-db = new Mongo().getDB("notification");
-db.createCollection("notification");
+db = new Mongo().getDB('notification')
+db.createCollection('notification')
 
+db.notification.insertMany([
+  {
+    id: 'test-notification-id',
+    type: 'outlier',
+    timestamp: new Date(),
+    event: {
+      id: 'test-event-id',
+      rangeRuleId: 'test-range-rule-id',
+      measurementId: 'test-measurement-id'
+    },
+    message: 'test-message'
+  }
+])

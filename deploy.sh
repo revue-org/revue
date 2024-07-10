a51f9fd06ac87333af11c56292a7679c3efb706d
Mattia Matteini <matteinimattia@gmail.com>
2024-07-10 17:55:50 +0200

    refactor: update deploy and undeploy scripts supporting k8s

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,50 @@
 # Function to display usage information
 usage() {
-  echo "Usage: $0 [--build]"
+  echo "Usage: $0 --docker [--build] | --k8s"
   exit 1
 }
 
-# if $1 exists and is not equal to "--build", display usage information
+# Function to check if Minikube is running
+check_minikube() {
+  status=$(minikube status --format='{{.Host}}')
+
+  if [ "$status" == "Running" ]; then
+    return 0
+  else
+    return 1
+  fi
+}
+
+# Check if no arguments were provided
 if [ $# -eq 0 ]; then
-  ./scripts/compose-all.sh --up -d
-elif [ "$1" != "--build" ]; then
   usage
-else
-  ./scripts/compose-all.sh --up -d --build
 fi
+
+# Determine the deployment type
+case "$1" in
+  --docker)
+    if [ "$2" == "--build" ]; then
+      ./scripts/compose-all.sh --up -d --build
+    elif [ -z "$2" ]; then
+      ./scripts/compose-all.sh --up -d
+    else
+      usage
+    fi
+    ;;
+  --k8s)
+    if [ -z "$2" ]; then
+      if check_minikube; then
+        echo "Minikube is already running."
+      else
+        echo "Starting Minikube..."
+        minikube start
+      fi
+      kubectl apply -f k8s -f gateway/k8s
+    else
+      usage
+    fi
+    ;;
+  *)
+    usage
+    ;;
+esac
--- undeploy.sh
+++ undeploy.sh
@@ -1,1 +1,40 @@
-./scripts/compose-all.sh --down
+# Function to display usage information
+usage() {
+  echo "Usage: $0 --docker | --k8s"
+  exit 1
+}
+
+# Function to check if Minikube is running
+check_minikube() {
+  status=$(minikube status --format='{{.Host}}')
+
+  if [ "$status" == "Running" ]; then
+    return 0
+  else
+    return 1
+  fi
+}
+
+# Check if no arguments were provided
+if [ $# -eq 0 ]; then
+  usage
+fi
+
+# Determine the deployment type
+case "$1" in
+  --docker)
+    ./scripts/compose-all.sh --down
+    ;;
+  --k8s)
+      if check_minikube; then
+        kubectl delete -f k8s -f gateway/k8s
+        minikube stop
+        echo "Cluster stopped."
+      else
+        echo "Cluster already stopped."
+      fi
+    ;;
+  *)
+    usage
+    ;;
+esac

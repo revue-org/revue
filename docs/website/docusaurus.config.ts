ce18c08ec9c53de667a5ebbbe50bbf7f3cbb3a78
Kelvin Olaiya <kelvinoluwada.olaiya@studio.unibo.it>
2024-07-22 17:04:51 +0200

    build(website): only warn on broken links

--- docs/website/docusaurus.config.ts
+++ docs/website/docusaurus.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
   organizationName: 'Revue', // Usually your GitHub org/user name.
   projectName: 'revue', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set

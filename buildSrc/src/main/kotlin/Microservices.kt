c109ddf64d249b375b45d5d91198f729c29894bb
Kelvin Olaiya <kelvinoluwada.olaiya@studio.unibo.it>
2024-08-06 11:48:26 +0200

    chore: minimal refactor

--- buildSrc/src/main/kotlin/Microservices.kt
+++ buildSrc/src/main/kotlin/Microservices.kt
@@ -1,3 +1,6 @@
+/**
+ * Enum class that contains all the microservices paths.
+ */
 enum class Microservices(
     val path: String,
 ) {
@@ -16,6 +19,10 @@ enum class Microservices(
     override fun toString(): String = path
 
     companion object {
+
+        /**
+         * Check if the tester is a microservice.
+         */
         fun isAMicroservice(tester: String) = tester.lowercase() in values().map { it.path }
     }
 }

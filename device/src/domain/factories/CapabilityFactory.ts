e850586ad872a0f25ffe030ffb81580aac7f776e
Mattia Matteini <matteinimattia@gmail.com>
2024-08-29 11:34:14 +0200

    chore(device): use VideoStreamingCapability (before unused)

--- device/src/domain/factories/CapabilityFactory.ts
+++ device/src/domain/factories/CapabilityFactory.ts
@@ -1,6 +1,7 @@
 import { CapabilityType } from '@/domain/core/capabilities/CapabilityType.js'
 import { Measure } from '@common/domain/core/Measure.js'
 import { SensoringCapability } from '@/domain/core/capabilities/SensoringCapability.js'
+import { VideoStreamingCapability } from "@/domain/core/capabilities/VideoStreamingCapability.js";
 
 export class CapabilityFactory {
   static sensoringCapabilityOf(capturingInterval: number, measure: Measure): SensoringCapability {
@@ -11,7 +12,7 @@ export class CapabilityFactory {
     }
   }
 
-  static videoStreamingCapabilityOf(resolution: '576p' | '720p' | '1080p' | '4k') {
+  static videoStreamingCapabilityOf(resolution: '576p' | '720p' | '1080p' | '4k'): VideoStreamingCapability {
     return {
       type: CapabilityType.VIDEO,
       resolution

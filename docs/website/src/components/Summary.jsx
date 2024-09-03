04d8bbe2a972611eefce7c4a97a26a2d99a5a4a8
Kelvin Olaiya <kelvinoluwada.olaiya@studio.unibo.it>
2024-09-03 11:49:17 +0200

    chore(website): augment the border radius is the summary component

--- docs/website/src/components/Summary.jsx
+++ docs/website/src/components/Summary.jsx
@@ -4,7 +4,7 @@ export const Summary = ({ children, title }) => {
             cursor: 'pointer',
             backgroundColor: '#e6eff5',
             padding: '25px',
-            borderRadius: '5px',
+            borderRadius: '25px',
             border: '1px solid #9dd2f5',
             marginTop: '25px',
             marginBottom: '25px',
